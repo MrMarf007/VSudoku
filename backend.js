44601f2747a1d15ba2f3f12f3ebdf9d813ee066c
MrMarf <ruiterstan@gmail.com>
2024-01-06 21:04:10 +0100

    setting

--- backend.js
+++ backend.js
@@ -6,7 +6,11 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
-const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
+const io = new Server(server, { 
+  pingInterval: 2000, 
+  pingTimeout: 5000,
+  connectionStateRecovery: {}
+})
 
 const port = 3000
 
